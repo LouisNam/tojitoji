dd74883126b86ae47c5677d97f8fecf5aa093f7b
Tee <dotranthainam@gmail.com>
2018-12-14 21:21:08 +0700

    #11
    Done warehouse and inventory transaction

---
+++ tojitoji.Model/Models/PurchaseOrder.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tojitoji.Model.Models
+{
+    [Table("PurchaseOrders")]
+    public class PurchaseOrder
+    {
+
+    }
+}
